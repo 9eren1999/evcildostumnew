cff6b0c91c5f1e0c89e904d7760450eb9cbd1799
Ali ihsan Balamur <alibalamur9@gmail.com>
2024-01-02 21:21:21 +0300

    xx

--- lib/ayarlarscreen/ayarlarpage.dart
+++ lib/ayarlarscreen/ayarlarpage.dart
@@ -19,16 +19,27 @@ class _AyarlarPageState extends State<AyarlarPage> {
       appBar: AppBar(
         title: Text('Ayarlar'),
         leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios_new_rounded, color: Colors.grey.shade800, size: 19,),
+          icon: Icon(
+            Icons.arrow_back_ios_new_rounded,
+            color: Colors.grey.shade800,
+            size: 19,
+          ),
           onPressed: () {
             Navigator.of(context).pop();
           },
         ),
       ),
       body: ListView(
-        children: [ SizedBox(height: 5,),
+        children: [
+          SizedBox(
+            height: 5,
+          ),
           ListTile(
-            title: Text('Hesaplar' ,style: TextStyle(fontWeight: FontWeight.w600, color: Colors.grey.shade900,)),
+            title: Text('Hesaplar',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.grey.shade900,
+                )),
             subtitle: Text(
               'Kişisel hesabını ya da evcil hayvan profillerini düzenle',
               style: TextStyle(color: Colors.grey.shade600),
@@ -36,26 +47,43 @@ class _AyarlarPageState extends State<AyarlarPage> {
           ),
           ListTile(
             title: Text('Profilim'),
-            trailing: Icon(Icons.arrow_forward_ios,  color: Colors.grey.shade800, size: 18,),
+            trailing: Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.grey.shade800,
+              size: 18,
+            ),
             onTap: () {
-                    Navigator.push(
+              Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => ProfilPage()), 
+                MaterialPageRoute(builder: (context) => ProfilPage()),
               );
             },
           ),
           ListTile(
             title: Text('Evcil Hayvan Profillerim'),
-            trailing: Icon(Icons.arrow_forward_ios, color: Colors.grey.shade800, size: 18,),
+            trailing: Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.grey.shade800,
+              size: 18,
+            ),
             onTap: () {
-                    Navigator.push(
+              Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => DostlarimPage()), 
+                MaterialPageRoute(builder: (context) => DostlarimPage()),
               );
             },
-          ),SizedBox(height: 10,), Divider(), SizedBox(height: 10,), 
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Divider(),
+          SizedBox(
+            height: 10,
+          ),
           ListTile(
-            title: Text('Bildirim ve İletişim' ,style: TextStyle(fontWeight: FontWeight.w600, color: Colors.grey.shade900)),
+            title: Text('Bildirim ve İletişim',
+                style: TextStyle(
+                    fontWeight: FontWeight.w600, color: Colors.grey.shade900)),
             subtitle: Text(
               'Uygulama bildirimlerini ve iletişim yöntemlerini kontrol et',
               style: TextStyle(color: Colors.grey.shade600),
@@ -78,23 +106,29 @@ class _AyarlarPageState extends State<AyarlarPage> {
                 isMailBildirimleriOn = newValue;
               });
             },
-          ), SizedBox(height: 10,), Divider(), SizedBox(height: 10,), 
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Divider(),
+          SizedBox(
+            height: 10,
+          ),
           ListTile(
-            title: Text('Şartlar ve Koşullar' ,style: TextStyle(fontWeight: FontWeight.w600, color: Colors.grey.shade900)),
+            title: Text('Şartlar ve Koşullar',
+                style: TextStyle(
+                    fontWeight: FontWeight.w600, color: Colors.grey.shade900)),
             subtitle: Text(
               'Uygulama hakları, veri işleme ve sınırlandırmaları keşfet',
               style: TextStyle(color: Colors.grey.shade600),
             ),
           ),
-
           ListTile(
             title: Text('Gizlilik Sözleşmesi'),
           ),
-
           ListTile(
             title: Text('Kullanıcı Sözleşmesi'),
           ),
-
           ListTile(
             title: Text('Kullanım Koşulları'),
           ),
